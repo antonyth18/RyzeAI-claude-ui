4ade6646647a8224c2937ea227c04acf3f2f488f
Antony <antonyth1818@gmail.com>
2026-02-14 20:41:27 +0530

    Fix production API URL

--- client/src/services/agentService.ts
+++ client/src/services/agentService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = import.meta.env.PROD ? '/api' : 'http://localhost:5001/api';
 
 export interface Message {
     id: number;
